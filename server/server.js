0f1417b09f8dd3bdf2a890ae77ba9e204242bdf7
roop <roopteja112@gmail.com>
2026-05-31 11:27:58 +0530

    Add: Intialize

---
+++ server/server.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const session = require("express-session");
+const MongoStore = require("connect-mongo").default;
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGO_URL,
+      touchAfter: 24 * 3600,
+    }),
+
+    cookie: {
+      secure: false,
+      httpOnly: true,
+      maxAge: 30 * 24 * 60 * 60 * 1000,
+    },
+  }),
+);
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("MongoDB Connected Successfully");
+  })
+  .catch((err) => {
+    console.log(err);
+    process.exit(1);
+  });
+
+app.get("/", (req, res) => {
+  return res.status(200).json({ message: "server started running" });
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`server is running on port ${process.env.PORT}`);
+});
